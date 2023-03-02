b81e16ed635b082053bb1be1eedab4a4f21b2d45
DanBaDo <byte4life@pm.me>
2023-03-02 07:07:28 +0100

    Usando React Router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { Link, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Local from './views/Local';
 import Portada from './views/Portada';
@@ -6,21 +6,23 @@ import Rexistro from './views/Rexistro';
 
 function App() {
 
-  const [ruta, setRuta] = useState("/")
-
   return (
     <>
       <h1>Periodico</h1>
+
       <nav>
         <ul>
-          <li onClick={()=>{setRuta("/")}}>Portada</li>
-          <li onClick={()=>{setRuta("/local/")}}>Local</li>
-          <li onClick={()=>{setRuta("/rexistro/")}}>Rexistro</li>
+          <li><Link to={"/"}>Portada</Link></li>
+          <li><Link to={"/local/"}>Local</Link></li>
+          <li><Link to={"/rexistro/"}>Rexistro</Link></li>
         </ul>
       </nav>
-      { ruta === "/" && <Portada/>}
-      { ruta === "/local/" && <Local/> }
-      { ruta === "/rexistro/" && <Rexistro/> } 
+      
+      <Routes>
+        <Route path='/' element={<Portada/>}/>
+        <Route path='/local/' element={<Local/>}/>
+        <Route path='/rexistro/' element={<Rexistro/>}/>
+      </Routes>
     </>
   );
 }
